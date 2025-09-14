d341341e75e3ffdc0d4292023dc968d11afa0513
Praju C <lpchedel@uwaterloo.ca>
2025-09-14 15:43:59 -0400

    Added basic navbar

---
+++ src/components/ui/navbar.js
@@ -0,0 +1,14 @@
+import "./navbar.css"
+
+export default function navbar(){
+    return (
+        <nav>
+            <div className="navbar-title nav-link">
+                vegan food app
+            </div>
+            <div className="nav-link">
+                Link
+            </div>
+        </nav>
+    );
+}
